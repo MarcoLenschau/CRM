27c18738826b24498d0226cd227c468fd88e6cbf
MarcoLenschau <contact@marco-lenschau.de>
2026-03-08 06:41:06 +0100

    style: Change hight from header and footer

--- app/components/Footer/Footer.tsx
+++ app/components/Footer/Footer.tsx
@@ -1,5 +1,5 @@
 export default function Footer() {
   return (
-    <footer className="h-24 w-full border-t-2 border-gray-600 bg-gray-900"></footer>
+    <footer className="h-31 w-full border-t-2 border-gray-600 bg-gray-900"></footer>
   );
 }
--- app/components/Header/Header.tsx
+++ app/components/Header/Header.tsx
@@ -2,7 +2,7 @@ import { Dispatch, SetStateAction } from "react";
 
 export default function Header({setSidebarShow, sidebarShow}: {setSidebarShow: Dispatch<SetStateAction<boolean>>, sidebarShow: boolean}) {
   return (
-    <header className="w-full h-24 flex items-center pl-8 bg-gray-900 border-b-2 border-gray-600">
+    <header className="w-full h-31 flex items-center pl-8 bg-gray-900 border-b-2 border-gray-600">
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" height="38" fill="currentColor" onClick={() => setSidebarShow(!sidebarShow)} className="cursor-pointer">
             <path d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5"/>
         </svg>
