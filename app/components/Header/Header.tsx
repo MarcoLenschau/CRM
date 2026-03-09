b7a1c5f8227bcf947e7cbaa63af63dc86b301ef7
MarcoLenschau <contact@marco-lenschau.de>
2026-03-09 02:48:35 +0100

    feat: Show the time in 12 hour format when the user change to english language

--- app/components/Header/Header.tsx
+++ app/components/Header/Header.tsx
@@ -1,16 +1,17 @@
-import { Dispatch, SetStateAction } from "react";
+import { Dispatch, SetStateAction, useState } from "react";
 import Time from "../Time/Time";
 
 import Language from "../Language/Language";
 
 export default function Header({setSidebarShow, sidebarShow}: {setSidebarShow: Dispatch<SetStateAction<boolean>>, sidebarShow: boolean}) {
+  const [hour12, setHourMode] = useState(false);
   return (
     <header className="flex justify-between items-center w-full h-31 pl-8 pr-8 border-b-2 border-gray-600 bg-zinc-900">
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" height="38" fill="currentColor" onClick={() => setSidebarShow(!sidebarShow)} className="cursor-pointer">
             <path d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5"/>
         </svg>
-        <Time />
-        <Language />
+        <Time hour12={hour12}/>
+        <Language setHourMode={setHourMode}/>
     </header>
   );
 }
--- app/components/Time/Time.tsx
+++ app/components/Time/Time.tsx
@@ -1,10 +1,13 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 
-export default function Time() {
-  const loadCurrentTime = () => new Date().toLocaleTimeString("de-DE", { hour: "2-digit", minute: "2-digit", second: "2-digit", hour12: false });
+export default function Time({hour12}: {hour12: boolean}) {
+  const loadCurrentTime = () => new Date().toLocaleTimeString("de-DE", { hour: "2-digit", minute: "2-digit", second: "2-digit", hour12: hour12 });
   const [time, setTime] = useState(loadCurrentTime); 
-  const interval = setInterval(() => setTime(loadCurrentTime()),1000); 
+  useEffect(() => {
+    const interval = setInterval(() => setTime(loadCurrentTime()), 1000);
+    return () => clearInterval(interval);
+  }, [hour12]);
 
   return (
     <time className="font-bold text-4xl">{time}</time>
