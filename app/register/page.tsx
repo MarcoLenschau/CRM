d5b5907c76180f15682e1aa89afb5c0c6ac0dcf5
MarcoLenschau <contact@marco-lenschau.de>
2026-03-09 05:30:41 +0100

    style: Add title and remove container from users page

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -2,12 +2,15 @@ import InputField from "../components/ui/InputField/InputField";
 import InputForm from "../components/ui/InputForm/InputForm";
 
 export default function Register() {
-  return (
+  return ( 
+    <>
+      <title>Register</title>
       <InputForm>
         <InputField placeholder="Email"/>
         <InputField placeholder="Name"/>
         <InputField placeholder="Password"/>
         <InputField placeholder="Password repeat"/>
       </InputForm>
+    </>
   );
 }
--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,9 +1,5 @@
-import Container from "../components/Container/Container";
-
 export default function Users() {
   return (
-    <Container>
       <h1>Users</h1>
-    </Container>
   );
 }
