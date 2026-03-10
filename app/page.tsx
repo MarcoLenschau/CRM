919031d34563545e8544bf73d77f55ffcbed2e63
MarcoLenschau <contact@marco-lenschau.de>
2026-03-10 05:03:48 +0100

    fix: Use compoent with client rendering

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import Button from "./components/ui/Button/Button";
 import InputField from "./components/ui/InputField/InputField";
 import InputForm from "./components/ui/InputForm/InputForm";
